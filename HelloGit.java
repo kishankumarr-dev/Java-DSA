89e937c6c2821cb622f3b17a7c528e9ec7ef12a8
Kishan Kumar R <kishankumarr2006@gmail.com>
2026-09-22 18:48:57 +0530

    restore reset practice

--- HelloGit.java
+++ HelloGit.java
@@ -3,6 +3,6 @@ public class HelloGit {
         System.out.println("Hello GitHub!");
         System.out.println("i am currectly learning git and github");
         System.out.println("i am learning gitand github practically");
-        
+        System.out.println("git reset practice");
     }
 }
