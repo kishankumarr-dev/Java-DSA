ea3892fc0a45d1616be8138103bc3383c34a1421
Kishan Kumar R <kishankumarr2006@gmail.com>
2026-09-22 17:50:23 +0530

    update hello git program

--- HelloGit.java
+++ HelloGit.java
@@ -2,5 +2,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello GitHub!");
         System.out.println("i am currectly learning git and github");
+        System.out.println("i am learning gitand github practically");
+        
     }
 }
